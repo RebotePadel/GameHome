974d81502f8f279951e70c3060c9d66791fa43f0
Anto PE2L <rebotepadelshop@gmail.com>
2026-01-08 08:00:25 +0100

    Add files via upload

--- start-gamehome.bat
+++ start-gamehome.bat
@@ -8,11 +8,7 @@ REM ---------- BACKEND ----------
 start cmd /k "cd /d %~dp0backend && npm run dev"
 
 REM ---------- OUVERTURE DU NAVIGATEUR ----------
-REM Ouvre l'URL dans Chrome (si tu veux forcer Chrome) :
+timeout /t 3 /nobreak >nul
 start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" "http://localhost:5173/"
 
-REM Si tu préfères utiliser le navigateur par défaut, remplace la ligne au-dessus par :
-REM start "" http://localhost:5173/
-
 echo GameHome est en train de se lancer dans le navigateur...
-pause
--- start-gamehome.vbs
+++ start-gamehome.vbs
@@ -1,3 +1,3 @@
 Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run chr(34) & "start-gamehome.bat" & Chr(34), 0
+WshShell.Run chr(34) & CreateObject("Scripting.FileSystemObject").GetParentFolderName(WScript.ScriptFullName) & "\start-gamehome.bat" & Chr(34), 0
 Set WshShell = Nothing
